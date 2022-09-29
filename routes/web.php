9ec1d6b5ad58997f2dc287b618ff44c5a7f484c5
shanjoysutradhar <shanjoy35-2025@diu.edu.bd>
2022-09-29 17:50:14 +0600

    3rd setup admin theme and menu items

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['prefix'=>'admin','middleware'=>'auth'],function(){
+    Route::get('/',[AdminController::class,'admin'])->name('admin');
+});
